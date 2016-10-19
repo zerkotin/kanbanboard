a9dd3f81e4d82990a1739e029c36a7cf7c23a000
Itzik Cohen <itzikca@Itziks-Air.fritz.box>
2016-10-19 16:41:11 +0200

    renaming of server files

--- server.js
+++ server.js
@@ -1,41 +1,32 @@
-//if you want a server, run 'npm install express' first
-//i didnt want to include it in the package.json since it has nothing to do with it
+//thats our server, it includes a simple file handler and proxy to our redmine queries
 var express = require('express');
-var request = require("request");
-var rmapi = require('./redmine_api.js')
+var redmine = require('./redmine-proxy.js');
+
 var app = express();
 
+mapRedmineQueries(); //maps redmine queries to http requests
 
-//redmine API proxy
-var rmconfig = {redmineRoot: 'http://project.osthus.com/'};
-app.get('/issues', function (req, res) {
-    if (rmapi.isApiKeyMissing(req, res)) {
-        return;
-    }
-    request({
-            url: rmconfig.redmineRoot + 'issues.json?key=' + req.query.key,
-            json: true
-        },
-        function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                rmapi.makeCustomFields(body);
-                res.json(rmapi.filterIssuesByTeam(body, req.query.team));
-            }
-            else {
-                res.json({error: body});
-            }
-        }
-    );
-});
+configSimpleWebServer(); //should be called last
 
-app.get('/', function(req, res) {
-   res.sendFile(__dirname + '/index.html')
-});
 
-app.get(/^(.+)$/, function(req, res){
-    res.sendFile(__dirname + req.params[0]);
-});
+function mapRedmineQueries() {
+  app.get('/issues', redmine.queries.teamIssues);
+}
 
-app.listen(9090, function() {
-  console.log('Server running on port 9090');
-});
+function configSimpleWebServer() {
+
+  //handles get to localhost
+  app.get('/', function(req, res) {
+     res.sendFile(__dirname + '/index.html')
+  });
+
+  //handles requests for files
+  app.get(/^(.+)$/, function(req, res){
+      res.sendFile(__dirname + req.params[0]);
+  });
+
+  //runs the web server
+  app.listen(9090, function() {
+    console.log('Server running on port 9090');
+  });
+}
